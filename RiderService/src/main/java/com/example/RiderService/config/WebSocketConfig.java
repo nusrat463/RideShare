913cd42004fcf3276eb87e0e3f02349387d2b375
nusrat.mim <nusrat.mim@fintechhub.com.bd>
2025-05-21 23:11:11 +0600

    Unused WebSocketHandler removed from WebSocketConfig

--- RiderService/src/main/java/com/example/RiderService/config/WebSocketConfig.java
+++ RiderService/src/main/java/com/example/RiderService/config/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.example.RiderService.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.WebSocketHandler;
@@ -12,10 +13,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-    @Autowired
-    private WebSocketHandler handler;
-
+    
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
